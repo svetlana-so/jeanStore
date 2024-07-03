57e81a6d680daa339ef0976c128f9733a0842e02
Svetlana Soboleva <sobolevasvetlanaalex@gmail.com>
2024-07-03 19:56:33 +0200

    feat: :sparkles: add product to db with dummy values for now

--- src/app/ui/products/add-form.tsx
+++ src/app/ui/products/add-form.tsx
@@ -5,8 +5,11 @@ import {
   type FileState,
 } from '@/app/components/MultiImageDropzone';
 import { useEdgeStore } from '@/app/lib/edgestore';
-import { useState, useActionState } from 'react';
-import { createProduct, State } from '@/app/lib/actions';
+import { useState } from 'react';
+import { createProduct } from '@/app/lib/actions';
+import type { FormFields} from '@/app/lib/actions';
+import { Button } from '../button';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 
 export default function Form() {
@@ -15,8 +18,7 @@ export default function Form() {
   const [isCancelled, setIsCancelled] = useState(false);
   const { edgestore } = useEdgeStore();
 
-  const initialState: State = { message: null, errors: {} };
-  
+  const {register, handleSubmit, setError, formState: {errors, isSubmitting}} = useForm<FormFields>()
 
   function updateFileProgress(key: string, progress: FileState['progress']) {
     setFileStates((fileStates) => {
@@ -42,22 +44,6 @@ export default function Form() {
     setUrls([]);
   }
 
-  async function handleSubmit() {
-    try {
-      for (const url of urls) {
-        await edgestore.publicFiles.confirmUpload({
-          url,
-        });
-      }
-     
-      setFileStates([]);
-      setUrls([]);
-      
-    } catch (err) {
-      console.error('Error confirming upload:', err);
-    }
-  }
-
   if (isCancelled) {
     return (
       <div className="m-6 flex flex-col items-center">
@@ -72,8 +58,33 @@ export default function Form() {
     );
   }
 
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+     /*  // Confirm uploads
+      for (const url of urls) {
+        await edgestore.publicFiles.confirmUpload({ url });
+      } */
+
+      const result = await createProduct(data);
+
+      if (!result.success) {
+       setError('root', {
+        type: 'manual',
+        message: result.message || 'Error creating product',
+       })
+      }
+    } catch (err) {
+      setError('root', {
+        type: 'manual',
+        message: 'Error confirming upload',
+      });
+    }
+  };
+
+
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
     <div>
       <MultiImageDropzone
         value={fileStates}
@@ -116,37 +127,35 @@ export default function Form() {
       />
       <div className="flex flex-col gap-2">
         <div>Brand</div>
-        <TextField name="brand"/>
+        <input
+        {...register('brand', {
+          required: 'Is required'
+        })}
+            type="text"
+            name="brand"
+            className="rounded border border-zinc-600 bg-zinc-100 px-2 py-1"
+          />
+          {errors.brand && (<div className='text-red-400'>{errors.brand.message}</div>)}
+          {errors.root && (<div className='text-red-400'>{errors.root.message}</div>)}
         <div className="mt-2 flex justify-end gap-2">
           <button
+
             className="rounded bg-white px-3 py-1 text-black hover:opacity-80"
             onClick={handleCancel}
           >
             Cancel
           </button>
-          <button
-            className="rounded bg-white px-3 py-1 text-black hover:opacity-80"
-            type="submit"
+          <Button
+          disabled= {isSubmitting}
+          type="submit"
           >
-            Submit
-          </button>
+            {isSubmitting? 'Loading' : 'Submit'}
+          </Button>
         </div>
+
       </div>
     </div>
     </form>
   );
 }
 
-function TextField(props: {
-  name?: string;
-  onChange?: (value: string) => void;
-}) {
-  return (
-    <input
-      type="text"
-      name={props.name}
-      className="rounded border border-zinc-600 bg-zinc-100 px-2 py-1"
-      onChange={(e) => props.onChange?.(e.target.value)}
-    />
-  );
-}
